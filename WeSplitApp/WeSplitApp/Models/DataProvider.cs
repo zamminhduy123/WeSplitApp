298ba91d79f338b2d431a7274d96bb3b5996e901
DuyTheCoGa <47093479+zamminhduy123@users.noreply.github.com>
2020-12-03 14:15:31 +0700

    rename homeUC->DetailUC

---
+++ WeSplitApp/WeSplitApp/Models/DataProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeSplitApp.Models;
+
+namespace WeSplitApp.Model
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                {
+                    _ins = new DataProvider();
+                }
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+
+        public WeSplitAppDBEntities DB { get; set; }
+
+        public DataProvider()
+        {
+            DB = new WeSplitAppDBEntities();
+        }
+
+    }
+}
