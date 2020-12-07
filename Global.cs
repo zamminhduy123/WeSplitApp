48c293d0c88fd8d4c1eb688b01b20e7a455ca9c6
DuyTheCoGa <47093479+zamminhduy123@users.noreply.github.com>
2020-12-07 14:10:52 +0700

    Fix Binding ThemeColor

---
+++ Global.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using WeSplitApp.ViewModels;
+
+namespace WeSplitApp
+{
+    public class Global : INotifyPropertyChanged
+    {
+        private static Global _instance = null;
+        private String _themeColor;
+        private BaseViewModel _currentPageViewModel = null; // Attribute - Backup fields
+        //Helper for Thread Safety
+        private static object m_lock = new object();
+
+        public static Global GetInstance()
+        {
+            // DoubleLock
+            if (_instance == null)
+            {
+                lock (m_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Global();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public BaseViewModel CurrentPageViewModel { get => _currentPageViewModel; set { _currentPageViewModel = value; OnPropertyChanged("CurrentPageViewModel"); } }
+
+        public String ThemeColor
+        {
+            get => _themeColor;
+            set
+            {
+                _themeColor = value; // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged("ThemeColor");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        Global()
+        {
+            CurrentPageViewModel = new HomeUCViewModel();
+            ThemeColor = "#03254c";
+        }
+    }
+}
---
+++ UserControls/ThemeUC.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Controls;
+using WeSplitApp.ViewModels;
+
+namespace WeSplitApp.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ThemeViewUC.xaml
+    /// </summary>
+    public partial class ThemeUC : UserControl
+    {
+        private ThemeUCViewModel Viewmodel { get; set; }
+
+        public ThemeUC()
+        {
+            InitializeComponent();
+            this.DataContext = Viewmodel = new ThemeUCViewModel();
+        }
+    }
+}
